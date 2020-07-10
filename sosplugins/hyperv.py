25249e29509d9de5f65eeeab1c8b1fa0bb2d4b54
Kameron Carr <kameroncarrcollege@gmail.com>
2020-07-10 00:27:48 +0000

    Hyper-V Plugin

---
+++ sosplugins/hyperv.py
@@ -0,0 +1,19 @@
+from sos.plugins import Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin
+
+
+class Hyperv(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
+    """Hyper-V client information"""
+
+    plugin_name="hyperv"
+
+    def setup(self):
+
+        self.add_copy_spec([
+            "/sys/bus/vmbus/drivers/",
+            "/sys/bus/vmbus/devices/*/*",
+            "/boot/config-*-azure"
+        ])
+
+        self.add_cmd_output("lsvmbus -vv")
+        self.add_cmd_output("lspci -vv")
+
