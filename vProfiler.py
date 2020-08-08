85e0c4d85b1f40c87f86db8e131baac196deb906
Kameron Carr <kameroncarrcollege@gmail.com>
2020-08-08 10:45:04 +0000

    YaML method [Testing]
    Implemented the plugin meta information using YaML instead of CSV.
    The commands only echo right now to for testing purposes.
    Actual plugin commands will be inserted after this is tested on every
    platform I am trying to support.

--- vProfiler.py
+++ vProfiler.py
@@ -1,42 +1,62 @@
 #!/usr/bin/env python3
-import argparse
+import yaml
 import os
+import argparse
+import re
+import sys
 
+def replace(string, substitutions):
+    substrings = sorted(substitutions, key=len, reverse=True)
+    regex = re.compile('|'.join(map(re.escape, substrings)))
+    return regex.sub(lambda match: substitutions[match.group(0)], string)
+
+LNX_DST = None
+try:
+    import distro
+    LNX_DST = distro.linux_distribution()[0]
+except ModuleNotFoundError:
+    try:
+        import platform
+        LNX_DST = platform.linux_distribution()[0]
+    except ModuleNotFoundError:
+        print("Could not determine Linux Distribution", "Please pip install distro or platform")
 
 parser = argparse.ArgumentParser()
-metavars = {}
 
-with open("plugins.csv", "r") as plugins:
-    for line in plugins:
-        if(line == "===\n"):
-            break
-        argument = [item.strip() for item in line.split(',')]
-        if(len(argument) < 2):
+with open("plugins.yaml", "r") as file:
+    plugins = yaml.load(file)
+    for name in plugins:
+        if name == "default":
             continue
-        elif(len(argument) == 2):
-            parser.add_argument("--"+argument[0], action="store_true")
+        plugin = plugins[name]
+        if "params" in plugin:
+            parser.add_argument("--"+name, nargs=len(plugin["params"]), help=plugin["description"], type=str, metavar=tuple(plugin["params"]))
         else:
-            metavars[argument[0]] = argument[2:]
-            parser.add_argument("--"+argument[0], nargs=len(argument[2:]), help=argument[1], type=str, metavar=tuple(argument[2:]))
-    args = vars(parser.parse_args())
+            parser.add_argument("--"+name, help=plugin["description"], action="store_true")
 
-    for line in plugins:
-        plugin = [item.strip() for item in line.split(',')]
-        if(len(plugin) < 2):
-            continue
-        elif(plugin[0] == ''):
-            print(plugin[1])
-            os.system(plugin[1])
-        elif(args[plugin[0]]):
-            command = plugin[1]
-            try:
-                for (key, value) in zip(metavars[plugin[0]], args[plugin[0]]):
-                    command = command.replace(key,value)
-            except KeyError:
-                pass
-            print(command)
-            os.system(command)
+args = vars(parser.parse_args())
+if len(sys.argv) == 1:
+    for name in plugins["default"]:
+        args[name] = plugins["default"][name]
 
-    
+for name in args:
+    if args[name] and "also_run" in plugins[name]:
+        for plg in plugins[name]["also_run"]:
+            args[plg] = plugins[name]["also_run"][plg]
 
-    
+for name in args:
+    if name == "default":
+        continue
+    plugin = plugins[name]
+    if "supported_distros" in plugin and not LNX_DST in plugin["supported_distros"]:
+        continue
+    if args[name] and "cmds" in plugin:
+        if "params" in plugin:
+            subs = {}
+            for (var, val) in zip(plugin["params"], args[name]):
+                subs[var] = val
+            for cmd in plugin["cmds"]:
+                os.system(replace(cmd, subs))
+        else:
+            for cmd in plugin["cmds"]:
+                os.system(cmd)
